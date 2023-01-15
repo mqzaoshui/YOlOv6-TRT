91e98c517fc1fde5ecc958ac4eb41a21fb554d64
xyh <Mao_Qi_UQ@163.com>
2023-01-16 00:03:38 +0800

    first commit

---
+++ TensorRT/build/CMakeFiles/yolov6.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yolov6.dir/yolov6.cpp.o"
+  "yolov6"
+  "yolov6.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/yolov6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
